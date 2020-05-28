e57681a346d644b2fbd05894899a99eff372ff00
Ryan Riegel <ryan.riegel@ibm.com>
2020-05-28 18:59:09 -0400

    Support multiple ranges for each SimpleSentence

--- socratic/theory.py
+++ socratic/theory.py
@@ -22,13 +22,15 @@ class SimpleSentence(Sentence):
     def __init__(self, formula, ranges):
         self.formula = formula
         self.ranges = ranges
+        self.active_range = None
 
     def configure(self, m):
         self.formula.configure(m)
 
-        m.add_constraint(self.formula.val >= self.ranges[0].lower)
-        m.add_constraint(self.formula.val <= self.ranges[0].upper)
-
+        self.active_range = m.binary_var_list(len(self.ranges))
+        m.add_indicators(self.active_range, (self.formula.val >= r.lower for r in self.ranges))
+        m.add_indicators(self.active_range, (self.formula.val <= r.upper for r in self.ranges))
+        m.add_constraint(m.sum(self.active_range) == 1)
 
 class RealRange(object):
     pass
