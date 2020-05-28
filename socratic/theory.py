63430f8c10e45baf27a5285937af7c40ba21bc75
Ryan Riegel <ryan.riegel@ibm.com>
2020-05-28 19:20:26 -0400

    Add support for open and half-open RealRanges

--- socratic/theory.py
+++ socratic/theory.py
@@ -7,8 +7,9 @@ class Theory(object):
         self.sentences = args
 
         self.m = mp.model.Model()
+        self.gap = self.m.continuous_var()
         for s in self.sentences:
-            s.configure(self.m)
+            s.configure(self.m, self.gap)
 
     def entails(self, query):
         pass
@@ -24,19 +25,52 @@ class SimpleSentence(Sentence):
         self.ranges = ranges
         self.active_range = None
 
-    def configure(self, m):
+    def configure(self, m, gap):
         self.formula.configure(m)
 
         self.active_range = m.binary_var_list(len(self.ranges))
-        m.add_indicators(self.active_range, (self.formula.val >= r.lower for r in self.ranges))
-        m.add_indicators(self.active_range, (self.formula.val <= r.upper for r in self.ranges))
         m.add_constraint(m.sum(self.active_range) == 1)
+        for i in range(len(self.ranges)):
+            self.ranges[i].configure(m, gap, self.formula, self.active_range[i])
+
 
 class RealRange(object):
-    pass
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
 
 
 class ClosedRange(RealRange):
     def __init__(self, lower, upper):
-        self.lower = lower
-        self.upper = upper
+        super().__init__(lower, upper)
+
+    def configure(self, m, gap, formula, active):
+        m.add_indicator(active, formula.val >= self.lower)
+        m.add_indicator(active, formula.val <= self.upper)
+
+
+class OpenRange(RealRange):
+    def __init__(self, lower, upper):
+        super().__init__(lower, upper)
+
+    def configure(self, m, gap, formula, active):
+        m.add_indicator(active, formula.val >= self.lower + gap)
+        m.add_indicator(active, formula.val <= self.upper - gap)
+
+
+class OpenLowerRange(RealRange):
+    def __init__(self, lower, upper):
+        super().__init__(lower, upper)
+
+    def configure(self, m, gap, formula, active):
+        m.add_indicator(active, formula.val >= self.lower + gap)
+        m.add_indicator(active, formula.val <= self.upper)
+
+
+class OpenUpperRange(RealRange):
+    def __init__(self, lower, upper):
+        super().__init__(lower, upper)
+
+    def configure(self, m, gap, formula, active):
+        m.add_indicator(active, formula.val >= self.lower)
+        m.add_indicator(active, formula.val <= self.upper - gap)
