ee055afc67aee4ca6c999fd6993e5d802ccbec51
Ryan Riegel <ryan.riegel@ibm.com>
2020-05-29 01:44:03 -0400

    Update demo to exhibit Formula/Theory reuse

--- socratic/demo.py
+++ socratic/demo.py
@@ -8,25 +8,28 @@ if __name__ == "__main__":
     dog = Prop("dog")
     pet = Prop("pet")
 
-    theory = Theory(
-        SimpleSentence(Implies(And(whiskers, tail), cat), [ClosedRange(.75, 1)]),
-        SimpleSentence(Implies(Or(cat, dog), pet), [ClosedRange(.75, 1)])
-    )
+    s1 = SimpleSentence(Implies(And(whiskers, tail), cat), [ClosedRange(.75, 1)])
+    s2 = SimpleSentence(Implies(Or(cat, dog), pet), [ClosedRange(.75, 1)])
+    s3 = SimpleSentence(Not(And(cat, dog)), [ClosedRange(1, 1)])
 
-    query = SimpleSentence(Implies(And(whiskers, Not(pet)), Not(tail)), [ClosedRange(.5, 1)])
+    theory1 = Theory(s1, s2)
+    theory2 = Theory(s1, s2, s3)
+
+    goal = Not(tail)
+
+    query1 = SimpleSentence(Implies(And(whiskers, Not(pet)), goal), [ClosedRange(.5, 1)])
+    query2 = SimpleSentence(Implies(And(whiskers, dog), goal), [ClosedRange(.5, 1)])
 
     import time
 
     t = time.time()
 
-    res = theory.entails(query)
+    print("theory1, query1: ", theory1.entails(query1))
+    print("theory1, query2: ", theory1.entails(query2))
+    print("theory2, query1: ", theory2.entails(query1))
+    print("theory2, query2: ", theory2.entails(query2))
 
     t = time.time() - t
 
-    print("res: ", res)
-    # if theory.m.solution:
-    #     print("gap: ", theory.gap.solution_value)
-    #     for prop in [whiskers, tail, cat, dog, pet]:
-    #         print("%s:  %f" % (prop.name, prop.val.solution_value))
     print()
     print("time: ", t)
