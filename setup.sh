5e2394a3f6d9ea818202ad8c74aee42b557cbdda
Ryan Riegel <ryan.riegel@ibm.com>
2021-10-25 20:03:45 -0400

    Remove a few useless comments from setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ PYTHON_VERSION=$(python3 -V | grep -o '[0-9]\+\.[0-9]\+')
 
 # Create the Python virtual environment if not already present
 if [[ ! -d venv ]]; then
-    # Attempt to install virtualenv (globally) if not found
+    # Attempt to install the virtualenv package (globally) if not found
     if ! python3 -m pip freeze | grep -q "^virtualenv=="; then
         python3 -m pip install virtualenv
     fi
@@ -18,10 +18,7 @@ if [[ ! -d venv ]]; then
     python3 -m virtualenv venv
 fi
 
-# Switch to the virtual environment's context
-. venv/bin/activate
-
-# Install/update required packages
+source venv/bin/activate
 pip install -r requirements.txt
 
 
@@ -30,25 +27,21 @@ CPLEX_PATH=cplex/python/$PYTHON_VERSION
 
 # Check for a CPLEX installation in the working directory
 # TODO: Also check platform-specific standard installer locations
-root_matches=("$CPLEX_ROOT"*)
+root_matches=("$CPLEX_ROOT"*)  # Glob
 if (( ${#root_matches[*]} != 1 )); then
-    # If not found (or not unique), prompt for installation location
     read -erp "Path to IBM ILOG CPLEX Optimization Studio ($CPLEX_ROOT*): " root_matches
 fi
 
 pushd "$root_matches/$CPLEX_PATH" >/dev/null
 
 # Check for available system architecture installers
-# TODO: Automatically select the appropriate installer if possible
-arch_matches=(*)
+arch_matches=(*)  # Glob
 if (( ${#arch_matches[*]} > 1 )); then
-    # If not unique, prompt for which to use
     echo "Multiple architecture installers detected:"
     printf '  %s\n' "${arch_matches[@]}"
     read -erp "Please select one of the above: " arch_matches
 fi
+
 cd "$arch_matches"
-
 python3 setup.py install
-
 popd >/dev/null
