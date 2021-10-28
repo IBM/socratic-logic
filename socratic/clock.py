bdd2a79def2d13ea326b88c1af6f3b0973d16f17
Ryan Riegel <ryan.riegel@ibm.com>
2021-10-28 12:49:31 -0400

    Add a docstring to clock.py

--- socratic/clock.py
+++ socratic/clock.py
@@ -1,14 +1,25 @@
-import time
+from time import time
+from typing import Callable
 
 
-def clock(fn, *args, **kwargs):
-    t = time.time()
+def clock(fn: Callable, *args, **kwargs):
+    """Print the running time of a function applied to the given arguments.
+
+    Example usage:
+        clock(time.sleep, 1)
+
+    :param fn: The function to be clocked.
+    :param args: Positional arguments for fn.
+    :param kwargs: Labelled arguments for fn.
+    :return: The return value of fn(*args, **kwargs).
+    """
+    t = time()
 
     try:
         ret = fn(*args, **kwargs)
 
     finally:
-        t = time.time() - t
+        t = time() - t
 
         print('fn:  ', fn.__name__, args, kwargs)
         print('time:', t)
